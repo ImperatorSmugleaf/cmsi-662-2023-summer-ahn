12e1fc4dee344a6ce531c066254437c5307ee6ff
Kieran Ahn <kieranahn@gmail.com>
2023-05-30 16:13:42 -0700

    Updated logic

--- Homework 2/C/flp32.c
+++ Homework 2/C/flp32.c
@@ -5,10 +5,8 @@ int main() {
     printf("Please input a number and receive the log of that number.\n");
     double number;
     scanf("%lf", &number);
-    if (isless(number, 0.0)) {
-        printf("Log function is not defined for numbers below 0.");
-    } else if (number == 0.0) {
-        printf("infinity");
+    if (islessequal(number, 0.0)) {
+        printf("Log function is not defined for zero and below.");
     } else {
         printf("%lf", log(number));
     }
