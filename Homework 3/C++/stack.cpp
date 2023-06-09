c5a29e3ea80cf36030bc2209fd95880ad668df5c
Kieran Ahn <kieranahn@gmail.com>
2023-06-09 04:47:26 -0700

    Fixed import semantics and updated unique pointer creation to make_unique

--- Homework 3/C++/stack.cpp
+++ Homework 3/C++/stack.cpp
@@ -4,86 +4,76 @@
 
 using namespace std;
 
-class StringStack {
-    static const unsigned int DEFAULT_CAPACITY = 10;
-    unique_ptr<unique_ptr<string>[]> frame;
-    unsigned int capacity;
-    unsigned int size;
-
-    void validate(const bool condition, const string message) {
-        if(!condition) {
-            cout<<message<<endl;
-            throw;
-        }
+void StringStack::validate(const bool condition, const string message) {
+    if(!condition) {
+        cout<<message<<endl;
+        throw;
     }
+}
 
-    void validateSizeNotZero() {
-        validate(this->size > 0, "Stack size must be greater than 0.");
+void StringStack::validateSizeNotZero() {
+    StringStack::validate(this->size > 0, "Stack size must be greater than 0.");
+}
+
+void StringStack::validateCanGrow() {
+    StringStack::validate(UINT_MAX - this->capacity > UINT_MAX - this->capacity * 2, "Maximum capacity reached.");
+}
+
+unsigned int StringStack::validatedCapacity(const unsigned int capacity) {
+    StringStack::validate(capacity > 0, "Capacity must be greater than 0.");
+    return capacity;
+}
+
+void StringStack::updateCapacity() {
+    unsigned int newCapacity = -1;
+    if(this->capacity >= this->size) {
+        StringStack::validateCanGrow();
+        unsigned int newCapacity = this->capacity * 2;
+    } else if (this->size * 4 <= this->capacity) {
+        unsigned int newCapacity = this->capacity / 2;
     }
-
-    void validateCanGrow() {
-        validate(UINT_MAX - this->capacity > UINT_MAX - this->capacity * 2, "Maximum capacity reached.");
+    if(newCapacity == -1) {
+        return;
     }
-
-    unsigned int validatedCapacity(const unsigned int capacity) {
-        validate(capacity > 0, "Capacity must be greater than 0.");
-        return capacity;
+    auto newFrame = make_unique<unique_ptr<string>[]>(newCapacity);
+    for(int i = 0; i < this->size; i++) {
+        newFrame[i].swap(this->frame[i]);
     }
+    this->capacity = newCapacity;
+    this->frame.swap(newFrame);
+}
 
-    void updateCapacity() {
-        unsigned int newCapacity = -1;
-        if(this->capacity >= this->size) {
-            validateCanGrow();
-            unsigned int newCapacity = this->capacity * 2;
-        } else if (this->size * 4 <= this->capacity) {
-            unsigned int newCapacity = this->capacity / 2;
-        }
+StringStack::StringStack() {
+    this->frame = make_unique<unique_ptr<string>[]>(StringStack::DEFAULT_CAPACITY);
+    this->capacity = DEFAULT_CAPACITY;
+    this->size = 0;
+}
 
-        if(newCapacity == -1) {
-            return;
-        }
+StringStack::StringStack(unsigned int userCapacity) {
+    this->frame = make_unique<unique_ptr<string>[]>(StringStack::validatedCapacity(userCapacity));
+    this->capacity = userCapacity;
+    this->size = 0;
+}
 
-        auto newFrame = make_unique<unique_ptr<string>[]>(newCapacity);
-        for(int i = 0; i < this->size; i++) {
-            newFrame[i].swap(this->frame[i]);
-        }
-        this->capacity = newCapacity;
-        this->frame.swap(newFrame);
-    }
+void StringStack::push(const string newElement) {
+    this->frame[this->size] = make_unique<string>(newElement);
+    size++;
+    StringStack::updateCapacity();
+}
 
-    public:
-    StringStack() {
-        this->frame = unique_ptr<unique_ptr<string>[]>(new unique_ptr<string>[DEFAULT_CAPACITY]);
-        this->capacity = DEFAULT_CAPACITY;
-        this->size = 0;
-    }
+string StringStack::pop() {
+    StringStack::validateSizeNotZero();
+    size--;
+    return *this->frame[this->size].release();
+}
 
-    StringStack(unsigned int userCapacity) {
-        this->frame = unique_ptr<unique_ptr<string>[]>(new unique_ptr<string>[validatedCapacity(userCapacity)]);
-        this->capacity = userCapacity;
-        this->size = 0;
-    }
+string StringStack::peek() {
+    return *this->frame[this->size-1].get();
+}
 
-    void push(const string newElement) {
-        this->frame[this->size] = unique_ptr<string>(new string(newElement));
-        size++;
-        updateCapacity();
-    }
-
-    string pop() {
-        validateSizeNotZero();
-        size--;
-        return *this->frame[this->size].release();
-    }
-
-    string peek() {
-        return *this->frame[this->size-1].get();
-    }
-
-    bool isEmpty() {
-        return this->size==0;
-    }
-};
+bool StringStack::isEmpty() {
+    return this->size==0;
+}
 
 int main() {
     printf("Existence\n");
