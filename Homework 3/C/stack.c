ac2f155c838a3a7c94f228459c4579709887a5cd
Kieran Ahn <kieranahn@gmail.com>
2023-06-08 17:25:48 -0700

    Updated C stack

--- Homework 3/C/stack.c
+++ Homework 3/C/stack.c
@@ -1,40 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "stack.h"
 
-typedef struct stringstack {
-    unsigned int capacity;
-    unsigned int size;
-    char** frame;
-} StringStack;
-
-StringStack* makeStringStack();
-void push(StringStack* stack, char* newElement);
-char* pop(StringStack* stack);
-void validateCapacity(StringStack* stack);
-void validateSizeNotZero(StringStack* stack);
-
+#define STACK_UNDERFLOW -1
+#define STACK_OVERFLOW -2
 
 int main() {
     return 0;
 }
 
-StringStack* makeStringStack() {
+StringStack* stack_create(unsigned int capacity) {
     StringStack* stack = (StringStack*)malloc(sizeof(struct stringstack));
-    stack->frame = malloc(sizeof(char*) * 10);
-    stack->capacity = 10;
-    stack->size = 0;
+    stack->items = (char**)malloc(sizeof(char*) * capacity);
+    stack->capacity = validatedCapacity(capacity);
+    stack->top = -1;
     return stack;
 }
 
-char* pop(StringStack* stack) {
+char* stack_pop(StringStack* stack) {
     //TODO: check if stack is empty
-    char* topElement = stack->frame[stack->size - 1];
-    stack->size--;
-    return topElement;
+    return stack->items[stack->top--];
 }
 
-void push(StringStack* stack, char* newElement) {
+void stack_push(StringStack* stack, char* newElement) {
+    stack->items[++stack->top] = newElement;
     //TODO: check if stack exceeds capacity, and update if so
-    stack->frame[stack->size] = newElement;
+}
+
+char* stack_peek(StringStack* stack) {
+    return stack->items[stack->top];
+}
+
+bool stack_is_empty(StringStack* stack) {
+    return stack->top == -1;
+}
+
+void stack_destroy(StringStack* stack) {
+    free(stack->items);
+    free(stack);
+}
+
+void updateCapacity(StringStack* stack) {
+    if(stack->capacity == stack->top + 1) {
+        stack->items = realloc(stack->items, sizeof(char*) * stack->capacity * 2);
+        stack->capacity *= 2;
+    } else if(stack->top + 1 * 4 < stack->capacity) {
+        stack->items = realloc(stack->items, sizeof(char*) * stack->capacity / 2);
+        stack->capacity = stack->capacity / 2;
+    }
 }
---
+++ Homework 3/C/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+
+#include <stdbool.h>
+
+typedef struct stringstack {
+    unsigned int capacity;
+    int top;
+    char** items;
+} StringStack;
+
+StringStack* stack_create(unsigned int capacity);
+void stack_push(StringStack* stack, char* newElement);
+char* stack_pop(StringStack* stack);
+char* stack_peek(StringStack* stack);
+void stack_destroy(StringStack* stack);
+bool stack_is_empty(StringStack* stack);
+unsigned int validatedCapacity(unsigned int capacity);
+void validateSizeNotZero(StringStack* stack);
+void updateCapacity(StringStack* stack);
+
+#endif
