ac4ab6cf4ad772651d51f6a83e34bf6e6323a245
Kieran Ahn <kieranahn@gmail.com>
2023-06-09 04:07:33 -0700

    Added new validation functions and changed function signatures for error checking

--- Homework 3/C/stack.h
+++ Homework 3/C/stack.h
@@ -1,7 +1,6 @@
 #ifndef STACK_H
 #define STACK_H
 
-
 #include <stdbool.h>
 
 typedef struct stringstack {
@@ -10,16 +9,17 @@ typedef struct stringstack {
     char** items;
 } StringStack;
 
-StringStack* stack_create(unsigned int capacity);
+StringStack* stack_create(int capacity);
 void stack_push(StringStack* stack, char* newElement);
 char* stack_pop(StringStack* stack);
 char* stack_peek(StringStack* stack);
 void stack_destroy(StringStack* stack);
 bool stack_is_empty(StringStack* stack);
-unsigned int validatedCapacity(unsigned int capacity);
+int validatedCapacity(int capacity);
 void validateSizeNotZero(StringStack* stack);
 void validateNotFull(StringStack* stack);
 void updateCapacity(StringStack* stack);
+void validateCanGrow(int capacity);
 
 
 #endif
