d87099864507449b2657fc94da4c741f6209a477
Kieran Ahn <kieranahn@gmail.com>
2023-06-09 04:47:39 -0700

    Updated header file

--- Homework 3/C++/stack.h
+++ Homework 3/C++/stack.h
@@ -2,14 +2,26 @@
 #define STACK_H
 
 #include <string>
+#include <memory>
 
 class StringStack {
+    static const unsigned int DEFAULT_CAPACITY = 10;
+    std::unique_ptr<std::unique_ptr<std::string>[]> frame;
+    unsigned int capacity;
+    unsigned int size;
+
+    void validate(const bool condition, const std::string message);
+    void validateSizeNotZero();
+    void validateCanGrow();
+    unsigned int validatedCapacity(unsigned int capacity);
+    void updateCapacity();
+
     public:
     StringStack();
     StringStack(unsigned int userCapacity);
-    void push(const string newElement);
-    string pop();
-    string peek();
+    void push(const std::string newElement);
+    std::string pop();
+    std::string peek();
     bool isEmpty();
 };
 
