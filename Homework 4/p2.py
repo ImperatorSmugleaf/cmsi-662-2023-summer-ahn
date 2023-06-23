b36baa9c935b39a30db3a6e6aafd59e80b249ffd
Kieran Ahn <kieranahn@gmail.com>
2023-06-23 07:07:53 -0700

    Adding solution for problem 2

---
+++ Homework 4/p2.py
@@ -0,0 +1,44 @@
+# Author: Kieran Ahn
+# A small demonstration of an Auto-Key Vigenère cipher using the ciphertext as
+# the key
+
+cipher = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+    "I": 8,
+    "J": 9,
+    "K": 10,
+    "L": 11,
+    "M": 12,
+    "N": 13,
+    "O": 14,
+    "P": 15,
+    "Q": 16,
+    "R": 17,
+    "S": 18,
+    "T": 19,
+    "U": 20,
+    "V": 21,
+    "W": 22,
+    "X": 23,
+    "Y": 24,
+    "Z": 25
+}
+
+plaintext = "TAKEACOPYOFYOURPOLICYTONORMAWILCOXONTHETHIRDFLOOR"
+key = "QUARK"
+ciphertext = ""
+for i in range(len(plaintext)):
+    encodedLetter = list(cipher.keys())[
+        (cipher[plaintext[i]] + cipher[key[i]]) % 25]
+    if len(key) < len(plaintext):
+        key += encodedLetter
+    ciphertext += encodedLetter
+print(f'KEY: {key}')
+print(f'CIPHERTEXT: {ciphertext}')
